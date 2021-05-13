6ed9a9e9f14a863976fd1f0e3b32e854ed84a8cb
nilson-roman <nilsonoroman@gmail.com>
2021-05-13 18:41:00 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsprojeto/DsprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprojetoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsprojeto/DsprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
