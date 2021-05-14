97d80c6d1116a28897dcd278ab749c009b24b8f4
nilson-roman <nilsonoroman@gmail.com>
2021-05-13 22:32:06 -0300

    Post new Client

--- backend/src/main/java/com/devsuperior/dsprojeto/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsprojeto/services/ClientService.java
@@ -31,4 +31,20 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
 		return new ClientDTO(entity);
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
 }
